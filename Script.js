55d0e70b8764e11709b02bd3878473df52582cb2
KARTHIKVSK17 <124972146+KARTHIKVSK17@users.noreply.github.com>
2023-03-13 12:48:00 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+function foo(){
+    var firstName=document.getElementById("FirstName").value;
+    console.log(firstName);
+    var middleName=document.getElementById("MiddleName").value;
+    console.log(middleName);
+    var lastName=document.getElementById("LastName").value;
+    console.log(lastName);
+    var res=document.getElementById("main").value;
+    console.log(res);
+    var res1=document.getElementById("Password").value;
+    console.log(res1);
+    }
+    
